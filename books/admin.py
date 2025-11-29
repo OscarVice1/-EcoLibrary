0f9527f13a84a9c5d0b0c4f22da61f059a9510a4
Oscar <ovicencioa@usm.cl>
2025-11-28 21:17:20 -0300

    Fase backend: Modelos,Admin,Staff

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book, Favorite
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "category", "description", "publication_date")
+    list_filter = ("title", "author")
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ("user", "book")
+    list_filter = ("user",)
--- books/models.py
+++ books/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(
+        max_length=200, verbose_name="Titulo", null=False, blank=False
+    )
+    author = models.CharField(
+        max_length=200, verbose_name="Autor", null=False, blank=False
+    )
+    category = models.CharField(
+        max_length=200, verbose_name="Categoria", null=False, blank=False
+    )
+    description = models.TextField(
+        verbose_name="Descripción breve", null=False, blank=False
+    )
+    publication_date = models.DateTimeField(auto_created=True, null=False, blank=False)
+    image = models.ImageField(
+        upload_to="books/",
+        null=True,
+        blank=True,
+        verbose_name="Portada",
+    )
+
+    class Meta:
+        verbose_name = "Libro"
+        verbose_name_plural = "Libros"
+
+        constraints = [models.UniqueConstraint(fields=["title"], name="unique_title")]
+
+    def __str__(self):
+        return self.title
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="favorites")
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name="favorited_by"
+    )
+
+    class Meta:
+        verbose_name = "Favorito"
+        verbose_name_plural = "Favoritos"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "book"], name="unique_user_book_favorite"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
