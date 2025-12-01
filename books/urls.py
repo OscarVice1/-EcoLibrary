3297dc3ec8d03ed435a2d11ea4870801a31f941b
Oscar <ovicencioa@usm.cl>
2025-12-01 13:13:24 -0300

    Favoritos implementado: Views,Templates,Urls

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,12 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("detalles/<int:book_id>/", views.book_detail, name="details"),
+    path(
+        "agregar-favorito/<int:book_id>/favorito/",
+        views.toggle_favorite,
+        name="toggle_favorite",
+    ),
+    path(
+        "mis-libros-favoritos", views.favorites_books_view, name="favorites_books_view"
+    ),
 ]
--- books/views.py
+++ books/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from typing import Set, List
 from .models import Book, Favorite
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import HttpRequest, HttpResponse
 from django.db.models import QuerySet
 
+
 # Create your views here.
+def _favorites_books(user: User) -> Set[int | None]:
+    """
+        Search the favorites book of the current user.
+    Args:
+        user (User): The user in the current sesion.
+
+    Returns:
+        Set[int | NONE]: id's of the favorites book of the current user or none.
+    """
+    favorites_id: Set[int | None] = set()
+    if user.is_authenticated:
+        favorites_id = set(
+            Favorite.objects.filter(user=user).values_list("book_id", flat=True)
+        )
+
+    return favorites_id
 
 
 def home(request: HttpRequest) -> HttpResponse:
@@ -20,8 +39,9 @@ def home(request: HttpRequest) -> HttpResponse:
         context containing the list of books ordered by creation date.
     """
     books: QuerySet[Book] = Book.objects.all().order_by("publication_date")
-
-    return render(request, "books/home.html", {"books": books})
+    favorites_id: Set[int] = _favorites_books(request.user)
+    context = {"books": books, "favorites_id": favorites_id}
+    return render(request, "books/home.html", context)
 
 
 def book_detail(request: HttpRequest, book_id: int) -> HttpResponse:
@@ -41,5 +61,45 @@ def book_detail(request: HttpRequest, book_id: int) -> HttpResponse:
     return render(request, "books/details.html", {"book": book})
 
 
-def add_book_favorite():
-    pass
+@login_required
+def toggle_favorite(request: HttpRequest, book_id: int) -> HttpResponse:
+    """
+    Toggle the current user's book favorite state.
+
+    Args:
+        request: The incoming HTTP request.
+        book_id: The ID of the book to toggle favorite status.
+
+    Returns:
+        Redirect to the previous page or home.
+    """
+
+    book: Book = get_object_or_404(Book, id=book_id)
+
+    is_favorite: bool = Favorite.objects.filter(user=request.user, book=book).exists()
+
+    if is_favorite:
+        Favorite.objects.filter(user=request.user, book=book).delete()
+        messages.info(request, f"'{book.title}' quitado de favoritos")
+    else:
+        Favorite.objects.create(user=request.user, book=book)
+        messages.success(request, f"'{book.title}' agregado a favoritos")
+
+    return redirect(request.META.get("HTTP_REFERER", "home"))
+
+
+@login_required
+def favorites_books_view(request: HttpRequest) -> HttpResponse:
+    """
+        Render the favorite book page of the current user.
+    Args:
+        request (HttpRequest): The incoming HTTP request.
+
+    Returns:
+        HttpResponse: Renders the 'books/favorites_book.html' with all the favorites books of user
+    """
+    favorites_books = Favorite.objects.filter(user=request.user).select_related("book")
+
+    return render(
+        request, "books/favorites_books.html", {"favorites_books": favorites_books}
+    )
